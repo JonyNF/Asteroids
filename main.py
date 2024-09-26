c21d80fdf9153c71a8c92ed2797f138fdce12754
JonyNF <jonynfores@gmail.com>
2024-09-26 14:19:24 +0200

    Sprite class implemented

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    #set GUI window
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
     clock = pygame.time.Clock()
     dt = 0
     
     while True:
+        #fill with black color
         screen.fill((0,0,0))
         
         #Close game process if screen closed
@@ -20,9 +23,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
+        #Refresh the screen
         pygame.display.flip()
         
-        
+        #force game to render at a max 60 fps so cpu consume is not excessive
         clock.tick(60) 
         dt = clock.tick()/60
         
