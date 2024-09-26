1674f66be83e050e26628e52b298391de33f997d
JonyNF <jonynfores@gmail.com>
2024-09-26 14:13:48 +0200

    PyGame clock/fps implemented

--- main.py
+++ main.py
@@ -9,14 +9,23 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    clock = pygame.time.Clock()
+    dt = 0
+    
     while True:
+        screen.fill((0,0,0))
+        
+        #Close game process if screen closed
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        screen.fill((0,0,0))
         
         pygame.display.flip()
+        
+        
+        clock.tick(60) 
+        dt = clock.tick()/60
+        
 
         
     
