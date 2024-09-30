fd1afef697cedfe5306767fd2c169afcf4ea544a
JonyNF <jonynfores@gmail.com>
2024-09-30 13:25:17 +0200

    Asteroids implemented

--- player.py
+++ player.py
@@ -6,7 +6,6 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
-        self.containers = []
     
     def draw(self, screen):
         points = self.triangle()
