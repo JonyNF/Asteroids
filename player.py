7dec32d47285d37094eb73a3948399564c261eb8
JonyNF <jonynfores@gmail.com>
2024-09-30 14:31:59 +0200

    Implemented bullets and shot rate limit

--- player.py
+++ player.py
@@ -1,11 +1,13 @@
 import pygame
 from constants import *
 from circleshape import CircleShape
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.__timer_ = 0
     
     def draw(self, screen):
         points = self.triangle()
@@ -27,9 +29,18 @@ class Player(CircleShape):
         forward = pygame.Vector2(0, -1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
         
+    def shoot(self, dt):
+        if self.__timer_ > 0:
+            return
+        self.__timer_ = PLAYER_SHOOT_COOLDOWN
+        shot = Shot(self.position[0], self.position[1],SHOT_RADIUS)
+        shot.velocity = pygame.Vector2(0, -1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        
+        
     def update(self, dt):
         keys = pygame.key.get_pressed()
-
+        self.__timer_ -= dt
+        
         if keys[pygame.K_a]:
             self.rotate(-dt)
         if keys[pygame.K_d]:
@@ -38,3 +49,5 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot(dt)
